70f0f75b149051ba641c425525ce0fc7d216247b
TheMalloum <malloum.laya@gmail.com>
2011-12-15 10:27:38 +0100

    Adding a DownloadController Action Class

---
+++ src/main/java/fr/issamax/essearch/action/DownloadController.java
@@ -0,0 +1,56 @@
+package fr.issamax.essearch.action;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.Base64;
+import org.elasticsearch.search.SearchHit;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("downloadController")
+@Scope("request")
+public class DownloadController {
+
+	@Autowired
+	Client esClient;
+
+	private SearchHit hit;
+	
+	private StreamedContent file;
+
+	public void setHit(SearchHit hit) {
+		this.hit = hit;
+		if (hit != null) {
+			try {
+				Map<String, Object> file = (Map<String, Object>) hit
+						.getSource().get("file");
+				String contentType = file.get("_content_type").toString();
+				String name = file.get("_name").toString();
+				String source64 = file.get("content").toString();
+				InputStream is = new ByteArrayInputStream(Base64.decode(source64));
+				this.file = new DefaultStreamedContent(is, contentType, name);
+			} catch (Throwable e) {
+				// Something goes wrong but we don't care ;-)
+			}
+		}
+	}
+
+	public StreamedContent getFile() {
+		return file;
+	}
+
+	public void setFile(StreamedContent file) {
+		this.file = file;
+	}
+
+	public SearchHit getHit() {
+		return hit;
+	}
+}
--- src/main/java/fr/issamax/essearch/data/Result.java
+++ src/main/java/fr/issamax/essearch/data/Result.java
@@ -1,35 +1,17 @@
 package fr.issamax.essearch.data;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
 
-import org.elasticsearch.common.Base64;
 import org.elasticsearch.search.SearchHit;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
 
 public class Result {
 	protected String title;
 	protected Collection<String> fragments=new ArrayList<String>();
 	protected SearchHit searchHit;
-    private StreamedContent file;  
 	
 	public Result(SearchHit searchHit) {
 		this.searchHit = searchHit;
-		try {
-			Map<String, Object> file =  (Map<String, Object>) searchHit.getSource().get("file");
-			String contentType = file.get("_content_type").toString();
-			String name = file.get("_name").toString();
-			String source64 = file.get("content").toString();
-			InputStream is = new ByteArrayInputStream(Base64.decode(source64));
-			this.file = new DefaultStreamedContent(is, contentType, name); 
-		} catch (Throwable e) {
-			// Something goes wrong but we don't care ;-)
-		}
 	}
 
 	public String getTitle() {
@@ -54,13 +36,5 @@ public class Result {
 	
 	public void setSearchHit(SearchHit searchHit) {
 		this.searchHit = searchHit;
-	}
-
-	public StreamedContent getFile() {
-		return file;
-	}
-
-	public void setFile(StreamedContent file) {
-		this.file = file;
 	}
 }
