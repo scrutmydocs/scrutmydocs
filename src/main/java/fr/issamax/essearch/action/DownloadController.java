03b5c7225ddf02f9896ebf1015b5aa1b329ad1ad
David Pilato <david@pilato.fr>
2011-12-15 13:20:55 +0100

    Remove warnings

--- src/main/java/fr/issamax/essearch/action/DownloadController.java
+++ src/main/java/fr/issamax/essearch/action/DownloadController.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Map;
 
-import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.Base64;
 import org.elasticsearch.search.SearchHit;
@@ -25,12 +24,12 @@ public class DownloadController {
 	
 	private StreamedContent file;
 
+	@SuppressWarnings("unchecked")
 	public void setHit(SearchHit hit) {
 		this.hit = hit;
 		if (hit != null) {
 			try {
-				Map<String, Object> file = (Map<String, Object>) hit
-						.getSource().get("file");
+				Map<String, Object> file = (Map<String, Object>) hit.getSource().get("file");
 				String contentType = file.get("_content_type").toString();
 				String name = file.get("_name").toString();
 				String source64 = file.get("content").toString();
