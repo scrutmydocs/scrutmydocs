6fdcd028caaa04dd652cfdc7a591df5db29b292e
David Pilato <david@pilato.fr>
2011-12-27 15:44:53 +0100

    Add I'm Lucky function

--- src/main/java/fr/issamax/essearch/action/SearchController.java
+++ src/main/java/fr/issamax/essearch/action/SearchController.java
@@ -5,10 +5,14 @@ import static org.elasticsearch.index.query.QueryBuilders.queryString;
 
 import java.io.Serializable;
 
+import javax.faces.context.FacesContext;
+
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -52,6 +56,34 @@ public class SearchController implements Serializable{
 
 	}
 
+	public void imlucky() {
+		try {
+			QueryBuilder qb;
+			if (search == null || search.trim().length() <= 0) {
+				qb = matchAllQuery();
+			} else {
+				qb = queryString(search);
+			}
+			
+			 SearchResponse searchResponse = esClient.prepareSearch()
+					.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+					.setQuery(qb).setFrom(0).setSize(10)
+					.addHighlightedField("name").addHighlightedField("file")
+					.setHighlighterPreTags("<b>")
+					.setHighlighterPostTags("</b>").execute().actionGet();
+
+			 SearchHits hits = searchResponse.getHits();
+			 
+			 if (hits != null && hits.getTotalHits() > 0) {
+				 SearchHit hit = hits.getAt(0);
+				FacesContext.getCurrentInstance().getExternalContext().redirect("api/download/"+hit.getId());
+			 }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
 	public void setSearch(String search) {
 		this.search = search;
 	}
@@ -67,5 +99,4 @@ public class SearchController implements Serializable{
 	public void setResults(Results results) {
 		this.results = results;
 	}
-
 }
