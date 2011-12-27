703045c110116a77304fbaae96b196165524b68f
David Pilato <david@pilato.fr>
2011-12-27 23:27:16 +0100

    Allow user creation : first commit... Needs a lot of improve !

--- src/main/java/fr/issamax/essearch/action/UserController.java
+++ src/main/java/fr/issamax/essearch/action/UserController.java
@@ -1,22 +1,36 @@
 package fr.issamax.essearch.action;
 
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.Base64;
 import org.primefaces.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import fr.issamax.essearch.data.User;
 
 @Component("userController")
 @Scope("session")
 public class UserController implements Serializable{
 
+	@Autowired
+	protected Client esClient;
+
+	@Deprecated
 	protected String username;
 	
+	@Deprecated
 	protected String password;
+	
+	protected User user = new User();
 	
 	protected boolean logged;
 	
@@ -44,18 +58,48 @@ public class UserController implements Serializable{
         FacesContext.getCurrentInstance().addMessage(null, msg);  
 	}
 
+	/**
+	 * Create or update the user
+	 */
+	public void push() {
+		// Persist the user
+		try {
+			esClient
+					.prepareIndex("users","user",user.getLogin())
+					.setSource(
+						jsonBuilder()
+							.startObject()
+								.field("login", user.getLogin())
+								.field("password", Base64
+									.encodeBytes(user.getPassword().getBytes()))
+								.field("email", user.getEmail())
+							.endObject())
+					.execute().actionGet();
+			FacesMessage msg = new FacesMessage("Succesful", user.getLogin() + " is created/updated.");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+	
+	@Deprecated
 	public String getUsername() {
 		return username;
 	}
 	
+	@Deprecated
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	
+	@Deprecated
 	public String getPassword() {
 		return password;
 	}
 
+	@Deprecated
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -68,6 +112,12 @@ public class UserController implements Serializable{
 		this.logged = logged;
 	}
 
+	public User getUser() {
+		return user;
+	}
 	
+	public void setUser(User user) {
+		this.user = user;
+	}
 	
 }
---
+++ src/main/java/fr/issamax/essearch/data/User.java
@@ -0,0 +1,45 @@
+package fr.issamax.essearch.data;
+
+public class User {
+	protected String login;
+	protected String password;
+	protected String email;
+
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login
+	 *            the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
