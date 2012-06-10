52b77ad5d5b2890c6e16f9387683503019440b48
malloum.laya@gmail.com <malloum.laya@gmail.com>
2012-06-10 22:12:29 +0200

    add : documents open in a other windows

--- src/main/java/fr/issamax/essearch/action/FileUploadController.java
+++ src/main/java/fr/issamax/essearch/action/FileUploadController.java
@@ -1,13 +1,18 @@
 package fr.issamax.essearch.action;
 
+import static fr.issamax.essearch.constant.ESSearchProperties.INDEX_NAME;
+import static fr.issamax.essearch.constant.ESSearchProperties.INDEX_TYPE_DOC;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
-import static fr.issamax.essearch.constant.ESSearchProperties.*;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
 
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.Base64;
 import org.primefaces.event.FileUploadEvent;
@@ -21,32 +26,82 @@ public class FileUploadController {
 
 	@Autowired
 	Client esClient;
+	
+	
+	String idDoc;
 
 	public void handleFileUpload(FileUploadEvent event) {
 		FacesMessage msg = new FacesMessage("Succesful", event.getFile()
 				.getFileName() + " is uploaded.");
 
 		try {
-			esClient
-					.prepareIndex(INDEX_NAME,INDEX_TYPE_DOC)
+			esClient.prepareIndex(INDEX_NAME, INDEX_TYPE_DOC)
 					.setSource(
-						jsonBuilder()
-							.startObject()
-								.field("name", event.getFile().getFileName())
-								.field("postDate", new Date())
-								.startObject("file")
-									.field("_content_type", event.getFile().getContentType())
-									.field("_name", event.getFile().getFileName())
-									.field("content", Base64
-											.encodeBytes(event.getFile().getContents()))
-								.endObject()
-							.endObject())
-					.execute().actionGet();
+							jsonBuilder()
+									.startObject()
+									.field("name",
+											event.getFile().getFileName())
+									.field("postDate", new Date())
+									.startObject("file")
+									.field("_content_type",
+											event.getFile().getContentType())
+									.field("_name",
+											event.getFile().getFileName())
+									.field("content",
+											Base64.encodeBytes(event.getFile()
+													.getContents()))
+									.endObject().endObject()).execute()
+					.actionGet();
 		} catch (Exception e) {
-			msg = new FacesMessage("Error", "Something went wrong with " + event.getFile()
-					.getFileName() + ". " + e.getMessage());
+			msg = new FacesMessage("Error", "Something went wrong with "
+					+ event.getFile().getFileName() + ". " + e.getMessage());
 		}
 
 		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void exportFile() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletResponse response = (HttpServletResponse) context
+				.getExternalContext().getResponse();
+
+		GetResponse esResponse = esClient
+				.prepareGet(INDEX_NAME, INDEX_TYPE_DOC, this.idDoc).execute()
+				.actionGet();
+		// if(!esResponse.isExists() ) {
+		// //TODO return a standard page who show a message like : this document
+		// is not available
+		// return null;
+		// }
+
+		@SuppressWarnings("unchecked")
+		Map<String, Object> attachment = (Map<String, Object>) esResponse
+				.getSource().get("file");
+
+		String contentType = (String) attachment.get("_content_type");
+
+		byte[] file;
+		try {
+			file = Base64.decode((String) attachment.get("content"));
+
+			response.setContentType(contentType);
+			// response.setHeader("Content-disposition",
+			// "inline=filename=file.pdf");
+
+			response.getOutputStream().write(file);
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+			context.responseComplete();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setIdDoc(String idDoc) {
+		this.idDoc = idDoc;
+	}
+	
+	public String getIdDoc() {
+		return idDoc;
 	}
 }
