90c5471a0849968364e61a288c7816bf730ba476
David Pilato <david@pilato.fr>
2012-06-10 22:40:06 +0200

    #2 : Extract the filesystem scanner to an Elasticsearch River

--- src/main/java/fr/issamax/essearch/configuration/AppConfig.java
+++ src/main/java/fr/issamax/essearch/configuration/AppConfig.java
@@ -1,10 +1,14 @@
 package fr.issamax.essearch.configuration;
 
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.node.Node;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import fr.issamax.essearch.constant.ESSearchProperties;
 import fr.pilato.spring.elasticsearch.ElasticsearchClientFactoryBean;
 import fr.pilato.spring.elasticsearch.ElasticsearchNodeFactoryBean;
 
@@ -22,6 +26,25 @@ public class AppConfig {
 		ElasticsearchClientFactoryBean factory = new ElasticsearchClientFactoryBean();
 		factory.setNode(esNode());
 		factory.afterPropertiesSet();
+		
+		// We are going to create the filesystem river if needed
+		XContentBuilder xb = jsonBuilder()
+				.startObject()
+					.field("type", "fs")
+					.startObject("fs")
+						.startArray("fs")
+							.startObject()
+								.field("name", "tmp")
+								.field("url", "/tmp")
+								.field("update_rate", 30 * 1000)
+							.endObject()
+						.endArray()
+					.endObject()
+				.endObject();		
+		
+		factory.getObject().prepareIndex("_river", ESSearchProperties.INDEX_NAME, "_meta").setSource(xb)
+				.execute().actionGet();
+		
 		return factory.getObject();
 	}
 }
