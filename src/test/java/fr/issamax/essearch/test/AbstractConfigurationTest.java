3f8b14d24b1b0666b8ee25670194ce95018b5265
David Pilato <david@pilato.fr>
2012-06-13 22:50:04 +0200

    Disable tests for abstract class

--- src/test/java/fr/issamax/essearch/test/AbstractConfigurationTest.java
+++ src/test/java/fr/issamax/essearch/test/AbstractConfigurationTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {
 		"classpath:annotation-context.xml"
 		})
-public class AbstractConfigurationTest {
+public abstract class AbstractConfigurationTest {
 	
 	@Autowired Node node;
 
